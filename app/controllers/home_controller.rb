c710268e88876648f74a5abb297dfcbf4949eae3
vishal1234568 <vishalrathod00143@gmail.com>
2022-12-03 15:13:04 +0530

    root to home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+    def index
+
+      render layout: false  
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  # get '/home'
+  resources :home
    
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    # Defines the root path route ("/")
+  #  resources :home
+
   root "html#index"
+
 end
